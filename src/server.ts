eaecdd80f32168cf800ffff08c80326a98c66035
Yuri Luiz <yuriluiz@live.com>
2026-02-06 12:09:43 -0300

    Fix: Alterado para browser distpath

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const distPath = path.join(__dirname, "dist/crud");
 app.use(express.static(distPath));
 
 app.use((req: Request, res: Response) => {
-    res.sendFile(path.join(distPath, "index.html"));
+    res.sendFile(path.join(distPath, "browser"));
 })
 
 app.listen(port, "0.0.0.0", () => {
